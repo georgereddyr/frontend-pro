69b1a29e7494bf2dd7e26db050f747ad167f3262
George <r0938474962@gmail.com>
2021-07-27 15:48:12 +0300

    hw1

---
+++ hw1/index.js
@@ -0,0 +1,39 @@
+let a = 5;
+let b = 3 * a;
+let c = a + b;
+console.log(a, b, c);
+
+
+let firstName = prompt('your first name', '');
+let lastName = prompt('your last name', '');
+alert("What's up, John Doe");
+
+
+let x = prompt('enter the first value', '');
+let y = prompt('enter the second value', '');
+alert(`${x*y}, ${x/y}, ${x+y}, ${x-y}`);
+
+
+let workingDays = prompt('Working days:', '');
+let workingHours = prompt('Working hours:', '');
+let salaryRate = prompt('Salary per hour:', '');
+alert(workingDays * workingHours * salaryRate);
+
+
+let odd = prompt('Gimme an odd number', '');
+if (odd % 2 == 0) {
+    alert(odd + ' is even!');
+} else {
+    alert(odd + ' is odd!');
+}
+
+
+let num = prompt('Gimme a number', '');
+if (isNaN(num)) {
+    alert(num + ' is NaN!');
+} else {
+    alert(num + ' is a number.');
+}
+
+
+let random = Math.random();
